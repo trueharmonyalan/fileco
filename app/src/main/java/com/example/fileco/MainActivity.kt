355cf2976e7124e88bafc176caab9c434db95f50
Alan <alanbiju2222hotmail@gmail.com>
2024-03-15 21:47:09 +0530

    Background Color Fixed "#27374D"

--- app/src/main/java/com/example/fileco/MainActivity.kt
+++ app/src/main/java/com/example/fileco/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.fileco
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fileco.ui.theme.FilecoTheme
 
@@ -17,9 +20,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FilecoTheme {
-
+                homeUi()
             }
         }
     }
 }
 
+@Composable
+fun homeUi() {
+
+    Column(
+
+        modifier = Modifier
+            .background(color = Color(android.graphics.Color.parseColor("#27374D")))
+            .fillMaxSize()
+    ) {
+
+
+    }
+
+
+
+}
