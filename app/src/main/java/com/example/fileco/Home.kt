211a1662049ed00371aa868b4f5bb3881a45eb15
Alan <alanbiju2222hotmail@gmail.com>
2024-03-17 11:19:52 +0530

    Navigation file updated with core code

--- app/src/main/java/com/example/fileco/Home.kt
+++ app/src/main/java/com/example/fileco/Home.kt
@@ -15,10 +15,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 
 @SuppressLint("SuspiciousIndentation")
 @Composable
-fun HomeUi() {
+fun HomeUi(navController: NavHostController) {
 
     val fontColor = Color(0xFF526D82)
 
--- app/src/main/java/com/example/fileco/Navigation.kt
+++ app/src/main/java/com/example/fileco/Navigation.kt
@@ -1,2 +1,23 @@
 package com.example.fileco
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavigationForFileCo() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "",
+    ){
+
+        composable(route="Home screen"){
+            HomeUi(navController)
+        }
+
+    }
+
+}
