931d4b916f3dc980ceaaf4a752b2c63d358938c6
Alan <alanbiju2222hotmail@gmail.com>
2024-03-17 12:31:34 +0530

    composable for filecompression window is done

--- app/src/main/java/com/example/fileco/MainActivity.kt
+++ app/src/main/java/com/example/fileco/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FilecoTheme {
-                NavigationForFileCo()
+                WindowFileCompression()
 
             }
         }
