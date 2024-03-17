96d3edb4d19c215c04603b64ab0bb26e816240eb
Alan <alanbiju2222hotmail@gmail.com>
2024-03-17 11:07:59 +0530

    Add separate file for Home

---
+++ app/src/main/java/com/example/fileco/Home.kt
@@ -0,0 +1,63 @@
+package com.example.fileco
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("SuspiciousIndentation")
+@Composable
+fun HomeUi() {
+
+    val fontColor = Color(0xFF526D82)
+
+    Box(
+
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(android.graphics.Color.parseColor("#27374D")))
+            .offset(x = 1.dp, y = 24.dp)
+
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)
+                .background(color = Color(android.graphics.Color.parseColor("#DDE6ED")))
+
+
+
+        ){
+
+
+            Text(
+                text = "Compressor",
+                color = fontColor,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier
+                    .offset(20.dp,4.dp)
+
+
+            )
+        }
+
+
+
+    }
+
+
+
+}
