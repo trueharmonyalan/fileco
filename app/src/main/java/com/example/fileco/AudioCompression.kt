897f2e823b1b6ddde796c1c13ee8c462a1c44269
Alan <alanbiju2222hotmail@gmail.com>
2024-03-17 12:38:48 +0530

    composable for audio, image and video is done by copying file compression window

--- app/src/main/java/com/example/fileco/AudioCompression.kt
+++ app/src/main/java/com/example/fileco/AudioCompression.kt
@@ -1,2 +1,213 @@
 package com.example.fileco
 
+import android.view.Window
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun WindowAudioCOmpression() {
+
+    val buttonStrokeColor = Color(0xFF9DB2BF)
+
+    Box(
+
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(android.graphics.Color.parseColor("#27374D")))
+            .offset(x = 1.dp, y = 2.dp)
+            .padding(10.dp)
+
+    ) {
+
+//logo for file compression
+        Column(
+            modifier = Modifier
+                .offset(x=30.dp,y= (60).dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .background(
+                        color = Color(android.graphics.Color.parseColor("#DDE6ED")),
+                        shape = RoundedCornerShape(5.dp)
+                    )
+
+
+
+            ){
+
+                // Logo Implemented in Rounded Rectangle for file compression
+                Image(painter = painterResource(id = R.drawable.graphic_eq_fill0_wght400_grad0_opsz24),
+                    contentDescription = "Image of Image compression",
+                    modifier = Modifier
+                        .offset(x = 5.dp, y = (5).dp)
+                        .size(40.dp)
+
+                )
+
+
+            }
+
+        }
+        //rounded rectangle position
+        Column(
+            modifier = Modifier
+
+
+                .offset(40.dp, 200.dp)
+                .padding(0.dp)
+        ) {
+
+            //rounded rectangle configuration
+
+            Column(
+                modifier = Modifier
+                    .height(296.dp)
+                    .width(296.dp)
+                    .background(
+                        color = Color(android.graphics.Color.parseColor("#17273e")),
+                        shape = RoundedCornerShape(15.dp)
+                    )
+                    .offset(x = 110.dp, y = 100.dp)
+                    .padding(0.dp)
+
+            ) {
+
+                //Icon inside rounded rectangle
+
+
+                Image(
+                    painter = painterResource(id = R.drawable.add_box_fill0_wght400_grad0_opsz24),
+                    contentDescription = "",
+                    colorFilter = ColorFilter.tint(Color.White),
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(0.dp)
+
+
+
+
+                )
+
+                //inside rounded rectangle text
+
+                Text(
+                    text ="Choose file",
+                    color = Color.White,
+
+                    modifier = Modifier
+                        .offset((-3).dp)
+
+                )
+
+
+
+            }
+        }
+
+
+        // Button Three is inside this Container
+        Column(
+
+            modifier = Modifier
+                .offset(25.dp, 530.dp)
+
+        ) {
+            //button design for Button Three
+            Column(
+                modifier = Modifier
+                    .height(68.dp)
+                    .width(321.dp)
+                    .background(
+                        color = Color(android.graphics.Color.parseColor("#526D82")),
+                        shape = RoundedCornerShape(60.dp)
+                    )
+                    .border(3.dp, color = buttonStrokeColor, shape = RoundedCornerShape(60.dp))
+
+
+            ) {
+                Text(
+                    text = "Quantity",
+                    fontSize = 25.sp,
+                    color = Color.White,
+                    letterSpacing = (-1).sp,
+                    fontWeight = FontWeight.Medium,
+
+                    modifier = Modifier
+                        .offset(x = 20.dp, y = 16.dp)
+
+                )
+            }
+
+        }
+
+
+        // Done Button
+
+        Column(
+
+            modifier = Modifier
+                .offset(25.dp, 650.dp)
+
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .height(68.dp)
+                    .width(321.dp)
+                    .background(
+                        color = Color(android.graphics.Color.parseColor("#526D82")),
+                        shape = RoundedCornerShape(60.dp)
+                    )
+                    .border(3.dp, color = buttonStrokeColor, shape = RoundedCornerShape(60.dp))
+
+
+            ) {
+                Text(
+
+                    text = "Done",
+                    fontSize = 25.sp,
+                    color = Color.White,
+                    letterSpacing = (-1).sp,
+                    fontWeight = FontWeight.Medium,
+
+                    modifier = Modifier
+                        .offset(x = 130.dp, y = 16.dp)
+
+                )
+            }
+
+        }
+    }
+
+
+}
+
+@Preview
+@Composable
+private fun WindowAudioCOmpressionPrev() {
+
+    WindowAudioCOmpression()
+
+}
